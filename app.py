6aade7a02ce80a234c945802545e3cdb6488850e
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-02-03 23:56:55 +0100

    update

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app= Flask (__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("reg")
+def register():
+    if request.method== "POST" and "username" in request.form and "email" in request.form and "password" in request.form:
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO #  VALUES (%s, %s, %s)', (username, password, email))
+        mysql.connection.commit()
+        msg = " "
+        return render_template('/')
+    elif request.method =="POST":
+        msg = " "
+        return render_template("/")
+
+
+
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
