ffa960d893a3a4cbb6bea9283e21385ca647b7bf
Lunar-Ahmed <145129924+Lunar-Ahmed@users.noreply.github.com>
2024-02-03 23:22:38 +0100

    update

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app= Flask (__name__)
 
 @app.route("/")
 def index():
-    return render_template('about.html')
+    return render_template('index.html')
 if __name__=="__main__":
     app.run(debug=True)
